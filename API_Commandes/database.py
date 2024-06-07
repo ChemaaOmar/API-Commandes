46c1003414c8adca640bc51dc88dc5db4f6308e9
paul-brd02 <paul.bardou@live.fr>
2024-06-07 10:29:02 +0200

    Postres conf update

--- API_Commandes/database.py
+++ API_Commandes/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Chaîne de connexion PostgreSQL
-SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost/dbname"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/Commandes"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
